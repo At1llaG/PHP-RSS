3678d9d48c6d47da872874202ac5860e51123524
Atilla Gündoğan <career@at1lla.com>
2022-06-08 22:35:54 +0300

    Added config for production

--- config/errors.php
+++ config/errors.php
@@ -1,5 +1,6 @@
 <?php
 
+    //Comment out the following lines in production
     ini_set('display_errors', 1);
     ini_set('log_erros',1);
     ini_set('error_log', dirname( dirname(__FILE__) ) . 'logs/log.txt');
